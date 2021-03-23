0010f7f7453d5f74edffba698c6828077da41f3f
jameslee <jameslee@kdanmobile.com>
2021-03-23 13:09:55 +0800

    memorize

--- countVowelStrings/countVowelStrings.js
+++ countVowelStrings/countVowelStrings.js
@@ -1,13 +1,10 @@
 bigHash = {}
 var countVowelStrings = function(n, currentNum=5) {
-  if(bigHash[n] == undefined) bigHash[n] = {}
   if(n == 1) return currentNum;
-  if(bigHash[n][currentNum]) return bigHash[n][currentNum]
   count = 0
   for(let i = 5; i >= 1; i--) {
     if(currentNum >= i) {
-      bigHash[n][currentNum] = countVowelStrings(n - 1, i)
-      count += bigHash[n][currentNum]
+      count += countVowelStrings(n - 1, i)
     }
   }
   return count
