06adec370d12be870f815d55e83b3304a74b9ced
James <jameslee@kdanmobile.com>
2021-03-25 22:43:46 +0800

    countVowelStrings [C++]

--- countVowelStrings/README.md
+++ countVowelStrings/README.md
@@ -1,1 +1,3 @@
 https://leetcode.com/problems/count-sorted-vowel-strings/
+
+There is a fuckin easy way but still too hard.
--- countVowelStrings/countVowelStrings.cpp
+++ countVowelStrings/countVowelStrings.cpp
@@ -1,23 +1,15 @@
 // https://leetcode.com/problems/count-sorted-vowel-strings/
 #include <iostream>
+#include <vector>
 using namespace std;
 class Solution {
   public:
-    int countVowelStrings(int n, int current_num=5) {
-      if(n == 1) return current_num;
-
-      int count = 0;
-      for (int i = 5; i >= 1; i--) {
-        count += countVowelStrings(n-1, i);
-      }
-
-      return count;
+    int countVowelStrings(int n)
+    {
+      vector<vector<int> > dp(n + 1, vector<int>(6));
+      for (int i = 1; i <= n; ++i)
+        for (int k = 1; k <= 5; ++k)
+          dp[i][k] = dp[i][k - 1] + (i > 1 ? dp[i - 1][k] : 1);
+      return dp[n][5];
     }
 };
-
-int main() {
-  Solution sol;
-  cout << sol.countVowelStrings(33) << endl; 
-  return 0;
-}
-
